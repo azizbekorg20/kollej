febacda3693e01582b8b3ed022a465095ab606fb
azizbekorg20 <hugedude113@gmail.com>
2026-09-08 20:12:22 +0500

    Update config.py

--- group_bot (1)/config.py
+++ group_bot (1)/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN', '').strip()
-ADMIN_IDS = {int(x.strip()) for x in os.getenv('ADMIN_IDS', '').split(',') if x.strip().isdigit()}
+BOT_TOKEN = os.getenv('8791199481:AAGC1ioPjF8zJCYH7S0MSCRvBgpclcB0b2U', '').strip()
+ADMIN_IDS = {int(x.strip()) for x in os.getenv('7328068181', '').split(',') if x.strip().isdigit()}
 DB_PATH = os.getenv('DB_PATH', 'bot.db')
 
 if not BOT_TOKEN:
